300a49bd17ec5eeaa1bdf361e0d0c449467db4e8
Gyan <gbhaskar89@gmail.com>
2020-05-08 12:04:33 +0530

    first commit

---
+++ EvenNumber.java
@@ -0,0 +1,13 @@
+class EvenNumber{
+public static void main (String args[]){
+
+for (int i=1;i<=10;i++){
+if(i%2==0){
+System.out.println(i +"is even no");
+}
+else {
+System.out.println("i is odd number");
+}
+}
+}
+}
